9e0302bdeef6c97be19c251f6279f18489ffff58
Amoralchik <dariysdemonad@gmail.com>
2021-11-29 22:09:29 +0200

    [css] Add tilewind 1/2

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -5,6 +5,7 @@
     </div>
     <div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio quae maxime ad perspiciatis, sed tempore in iure, libero veritatis natus dolores quasi pariatur perferendis quis repellat aut quod error.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis eaque veritatis ducimus? Quae nemo ullam, illum voluptas odit ex, voluptatum recusandae delectus ea, impedit dolorem voluptatem at possimus. Non, quaerat.</p>
     </div>
   </div>
 </template>
